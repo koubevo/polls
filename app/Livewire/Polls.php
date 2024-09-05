263ddb0625daafd3778f78af96e21832133223df
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2024-09-06 01:09:08 +0200

    voting

--- app/Livewire/Polls.php
+++ app/Livewire/Polls.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Option;
 use App\Models\Poll;
 use Livewire\Component;
 
@@ -18,6 +19,8 @@ class Polls extends Component
         return view('livewire.polls', ['polls' => $polls]);
     }
 
-
-
+    public function vote(Option $option)
+    {
+        $option->votes()->create();
+    }
 }
